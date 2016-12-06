ab98dec03f38e0a5ac18aabb43db6d05d8d65610
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-12-06 20:15:01 +0900

    Implement the REPL

---
+++ untypedi/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Language.LambdaCalculus.Context
+import Language.LambdaCalculus.Evaluator
+import Language.LambdaCalculus.Parser
+import Language.LambdaCalculus.Pretty
+
+import System.Console.Haskeline
+
+loop :: InputT IO ()
+loop = do
+  minput <- getInputLine "% "
+  case minput of
+    Nothing -> return ()
+    Just input -> case parseLC input of
+      Left _ -> do
+        outputStrLn $ "Invalid lambda caluclus: " ++ input
+        loop
+      Right p -> do
+        let term = eval [] p
+        outputStrLn $ printTm [] term
+        loop
+
+main :: IO ()
+main = runInputT defaultSettings loop
