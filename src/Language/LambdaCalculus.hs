9e754ee125dff9d9efaa3f425357cb8403d517b1
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-12-15 11:28:08 +0900

    Add run function

--- src/Language/LambdaCalculus.hs
+++ src/Language/LambdaCalculus.hs
@@ -3,9 +3,17 @@
 --
 module Language.LambdaCalculus
   ( module LC
+  , run
   ) where
 
+import Language.LambdaCalculus.AST as LC
 import Language.LambdaCalculus.Context as LC
 import Language.LambdaCalculus.Parser as LC
 import Language.LambdaCalculus.Evaluator as LC
 import Language.LambdaCalculus.Pretty as LC
+
+run :: String -> Either String String
+run script =
+   case parseLC script of
+     Left e -> Left (show e)
+     Right p -> Right (printTm [] (eval [] p))
